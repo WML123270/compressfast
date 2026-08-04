ffba85ffc9a841148139f661f2bd78677704235b
Meiliang Wu <756971388@qq.com>
2026-08-04 13:47:11 +0800

    fix: simplify BingSiteAuth matcher pattern

--- middleware.ts
+++ middleware.ts
@@ -164,5 +164,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|BingSiteAuth\.xml).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|BingSiteAuth).*)'],
 }
