bdbd9bb84cc110d926fde1295c9036b0b563f84f
Meiliang Wu <756971388@qq.com>
2026-08-07 23:10:48 +0800

    chore: update ECS deploy package timestamp to 20260807-230900

--- scripts/deploy-to-ecs.js
+++ scripts/deploy-to-ecs.js
@@ -19,8 +19,8 @@ const SERVER = {
   },
 };
 
-const localFile = 'deploy-package-20260806-232114.tar.gz';
-const remoteFile = '/home/admin/deploy-package-20260806-232114.tar.gz';
+const localFile = 'deploy-package-20260807-230900.tar.gz';
+const remoteFile = '/home/admin/deploy-package-20260807-230900.tar.gz';
 
 console.log('>>> 连接服务器...');
 const conn = new Client();
